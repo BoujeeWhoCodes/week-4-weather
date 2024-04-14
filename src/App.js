249bf8ba5268661efe29de917c43b3ca0d68f6f6
BoujeeWhoCodes <imellettis28@gmail.com>
2024-04-14 16:48:33 +0800

    Initial commit of src folder

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './styles.css';
+import Weather from './Weather';
+
+export default function App() {
+	return (
+		<div className='App'>
+			<h1 className='heading1'>Bougié Weather App 💅</h1>
+			<Weather />
+		</div>
+	);
+}
